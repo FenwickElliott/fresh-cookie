e50a2d14a0049e10088132007e1cc6c46239e5ab
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-05-10 09:10:42 -0400

    util

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"time"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -68,17 +65,5 @@ func in(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, err.Error())
 	} else {
 		io.WriteString(w, "Added to database")
-	}
-}
-
-func setNativeCookie(w *http.ResponseWriter, r *http.Request) {
-	h := sha1.New()
-	h.Write([]byte(time.Now().String() + r.RemoteAddr))
-	http.SetCookie(*w, &http.Cookie{Name: "nativeID", Value: hex.EncodeToString(h.Sum(nil)), Expires: time.Now().Add(365 * 24 * time.Hour)})
-}
-
-func check(err error) {
-	if err != nil {
-		log.Print(err)
 	}
 }
