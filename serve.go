121732d633e0d045a8cfe5c7ca36b45a44cfb861
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-05-09 20:51:59 -0400

    checking and setting native cookie

--- serve.go
+++ serve.go
@@ -1,23 +1,69 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
+	"time"
+
+	"gopkg.in/mgo.v2"
 )
 
+var (
+	err      error
+	c        *mgo.Collection
+	partners []string
+)
+
+type doc struct {
+	NativeID  string
+	ForeignID string
+	PartnerID string
+}
+
 func main() {
-	port := os.Getenv("port")
-	if port == "" {
-		port = "80"
-	}
+	partners = append(partners, "inception")
+	port := flag.String("port", "80", "port")
+	mongoServer := flag.String("mongoServer", "127.0.0.1", "Mongo Server Address")
+	flag.Parse()
+
+	session, err := mgo.Dial(*mongoServer)
+	check(err)
+	defer session.Close()
+	c = session.DB("db").C("collection")
+
 	http.HandleFunc("/", root)
-	fmt.Println("Listening on port:", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	fmt.Println("Listening on port:", *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
+	nativeID, err := r.Cookie("nativeID")
+	if nativeID == nil {
+		fmt.Println("Seting new native cookie")
+		http.SetCookie(w, &http.Cookie{Name: "nativeID", Value: newID(r), Expires: time.Now().Add(365 * 24 * time.Hour)})
+	} else {
+		check(err)
+	}
 	io.WriteString(w, "Hello, I'm the root\n")
+}
+
+func newID(r *http.Request) string {
+	h := sha1.New()
+	rand := time.Now().String()
+	for _, c := range r.Cookies() {
+		rand += c.Value
+	}
+	h.Write([]byte(rand))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func check(err error) {
+	if err != nil {
+		log.Print(err)
+	}
 }
